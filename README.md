08702e477592e95a7e0a31133ee98c8e81e4ae93
norvyll <keseruimidr@gmail.com>
2021-09-18 20:45:51 +0200

    changed the api and codes, add icons

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # weather-app
-Weather app with darksky api.
+
+1./ Simple weather app, use https://www.weatherbit.io/ and use Skycons icons.
+
+2./ Changed the weatherbit (day and night icons) icons to Skycons.
+
+3./ Click to the temperature, calculate to °C and degree °F.
+
+4./ Icons are moving.
+
+![](weather-app.gif)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-    var lat;
-    var long;
+    let lat;
+    let long;
 
     if (navigator.geolocation) {
 
@@ -10,21 +10,23 @@ $(document).ready(function () {
             lat = position.coords.latitude;
             long = position.coords.longitude;
 
-            var api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
+            let api = 'https://api.weatherbit.io/v2.0/current?lat=' + lat + '&lon=' + long + '&lang=hu&key=2b92554e16bc4061805540fec8eaeaca';
 
             $.getJSON(api, function (res) {
 
-                var celsius = res.main.temp;
-                var farenheit = (celsius * 1.8) + 32;
+                let celsius = res.data[0].temp;
+                let farenheit = (celsius * 1.8) + 32;
 
-                var location = res.name;
-                
+                let location = res.data[0].city_name;
+                //*console.log(res);*//
+
+
 
 
                 $('.weather-location').html(location);
                 $('.temp').html(Math.floor(celsius));
-                $('.weather-description').html(res.weather[0].description);
-                $('.weatherType').attr('id', res.weather[0].main);
+                $('.weather-description').html(res.data[0].weather.description);
+                $('.weatherType').attr('id', res.data[0].weather.icon);
                 $('.row2').on('click', function () {
                     if ($('.temp').html() == (Math.floor(celsius))) {
                         $('.temp').html(Math.floor(farenheit));
@@ -35,23 +37,69 @@ $(document).ready(function () {
                         $('.temp-type').html('°C');
                     }
                 });
-
+                $('.sunrise').html(res.data[0].sunrise);
 
                 //SETTING UP THE ICON 
-                var icons = new Skycons({
+                let icons = new Skycons({
                     "color": "white"
                 });
 
-                icons.set("Clear", Skycons.CLEAR_DAY);
-                icons.set("Clear-night", Skycons.CLEAR_NIGHT);
-                icons.set("Partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
-                icons.set("Partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
-                icons.set("Clouds", Skycons.CLOUDY);
-                icons.set("Rain", Skycons.RAIN);
-                icons.set("Sleet", Skycons.SLEET);
-                icons.set("Snow", Skycons.SNOW);
-                icons.set("Wind", Skycons.WIND);
-                icons.set("Fog", Skycons.FOG);
+                icons.set("c01d", Skycons.CLEAR_DAY);
+                icons.set("c01n", Skycons.CLEAR_DAY);
+                icons.set("c01d", Skycons.CLEAR_NIGHT);
+                icons.set("c01n", Skycons.CLEAR_NIGHT);
+                icons.set("c02d", Skycons.PARTLY_CLOUDY_DAY);
+                icons.set("c02n", Skycons.PARTLY_CLOUDY_NIGHT);
+                icons.set("c03d", Skycons.CLOUDY);
+                icons.set("c03n", Skycons.CLOUDY);
+                icons.set("c04d", Skycons.CLOUDY);
+                icons.set("c04n", Skycons.CLOUDY);
+                icons.set("t01d", Skycons.RAIN);
+                icons.set("t01n", Skycons.RAIN);
+                icons.set("t02d", Skycons.RAIN);
+                icons.set("t02n", Skycons.RAIN);
+                icons.set("t03d", Skycons.RAIN);
+                icons.set("t03n", Skycons.RAIN);
+                icons.set("t04d", Skycons.RAIN);
+                icons.set("t04n", Skycons.RAIN);
+                icons.set("t05d", Skycons.RAIN);
+                icons.set("t05n", Skycons.RAIN);
+                icons.set("r01d", Skycons.RAIN);
+                icons.set("r01n", Skycons.RAIN);
+                icons.set("r02d", Skycons.RAIN);
+                icons.set("r03n", Skycons.RAIN);
+                icons.set("f01d", Skycons.RAIN);
+                icons.set("f01n", Skycons.RAIN);
+                icons.set("r04d", Skycons.RAIN);
+                icons.set("r04n", Skycons.RAIN);
+                icons.set("r05d", Skycons.RAIN);
+                icons.set("r05n", Skycons.RAIN);
+                icons.set("r06d", Skycons.RAIN);
+                icons.set("r06n", Skycons.RAIN);
+                icons.set("s04d", Skycons.WIND);
+                icons.set("s04n", Skycons.WIND);
+                icons.set("s05d", Skycons.WIND);
+                icons.set("s05n", Skycons.WIND);
+                icons.set("s01d", Skycons.SNOW);
+                icons.set("s01n", Skycons.SNOW);
+                icons.set("s02d", Skycons.SNOW);
+                icons.set("s02n", Skycons.SNOW);
+                icons.set("s003d", Skycons.SNOW);
+                icons.set("s003n", Skycons.SNOW);
+                icons.set("s06d", Skycons.SNOW);
+                icons.set("s06n", Skycons.SNOW);
+                icons.set("a01d", Skycons.FOG);
+                icons.set("a01n", Skycons.FOG);
+                icons.set("a02d", Skycons.FOG);
+                icons.set("a02n", Skycons.FOG);
+                icons.set("a03d", Skycons.FOG);
+                icons.set("a03n", Skycons.FOG);
+                icons.set("a04d", Skycons.FOG);
+                icons.set("a04n", Skycons.FOG);
+                icons.set("a05d", Skycons.FOG);
+                icons.set("a05n", Skycons.FOG);
+                icons.set("a06d", Skycons.FOG);
+                icons.set("a06n", Skycons.FOG);
                 icons.play();
 
             });
